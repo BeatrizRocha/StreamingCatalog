ca445b2dbe23722bba3215dba20874f5472a0d06
Bea <beatriz@apoia.se>
2026-04-28 00:09:29 -0300

    feat(core): setup env validation with joi

--- catalog-api/src/app.module.ts
+++ catalog-api/src/app.module.ts
@@ -6,9 +6,19 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 
+import * as Joi from 'joi';
+
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        DATABASE_URL: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRES_IN: Joi.string().default('1d'),
+      }),
+    }),
     UserModule,
     AuthModule,
     PrismaModule,
